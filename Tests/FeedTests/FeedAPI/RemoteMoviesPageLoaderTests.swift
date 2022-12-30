81680fbf66e88a8260355a71cbfd2d3a0654fa0b
Paul Willis <ios_dev@paulrobertwillis.com>
2022-12-30 17:07:12 +0000

    add failure() helper function to improve test readability

--- Tests/FeedTests/FeedAPI/RemoteMoviesPageLoaderTests.swift
+++ Tests/FeedTests/FeedAPI/RemoteMoviesPageLoaderTests.swift
@@ -33,7 +33,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_whenReceivesClientError_shouldDeliverError() {
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteWith: .failure(RemoteMoviesPageLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
@@ -45,7 +45,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let statusCodeSamples = [199, 201, 300, 400, 500]
         
         statusCodeSamples.enumerated().forEach { index, statusCode in
-            expect(sut, toCompleteWith: .failure(RemoteMoviesPageLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let emptyMoviesPage = makeMoviesPageWithData()
                 client.complete(withStatusCode: statusCode, data: emptyMoviesPage.data, at: index)
             })
@@ -55,7 +55,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_whenReceives200HTTPResponseWithInvalidJSON_shouldDeliverError() {
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteWith: .failure(RemoteMoviesPageLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let invalidJson = Data("invalid data".utf8)
             client.complete(withStatusCode: 200, data: invalidJson)
         })
@@ -174,6 +174,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func failure(_ error: RemoteMoviesPageLoader.Error) -> RemoteMoviesPageLoader.Result {
+        .failure(error)
+    }
     
     private func trackForMemoryLeaks(
         _ instance: AnyObject,
