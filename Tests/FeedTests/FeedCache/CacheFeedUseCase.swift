e7403ca1b85ddcec0fb079470b954722660cf0d6
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-02 20:48:21 +0000

    Save command requests cache deletion

--- Tests/FeedTests/FeedCache/CacheFeedUseCase.swift
+++ Tests/FeedTests/FeedCache/CacheFeedUseCase.swift
@@ -1,21 +1,69 @@
+import Feed
 import XCTest
 
 class LocalFeedLoader {
+    
+    // MARK: - Private Properties
+    
+    private let store: FeedStore
+    
+    // MARK: - Init
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    // MARK: - API
+    
+    func save(_ moviesPage: MoviesPage) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
-    let deleteCachedFeedCallCount = 0
+    var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCase: XCTestCase {
     
-    func test_init_shouldNotDeleteCacheUponInitialisation() {
+    func test_init_whenCalled_shouldNotDeleteCacheUponInitialisation() {
         let store = FeedStore()
-        let _ = LocalFeedLoader(store: store)
+        _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_whenCalled_shouldRequestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        sut.save(uniqueMoviesPage())
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+
+    private func uniqueMoviesPage() -> MoviesPage {
+        let totalPages: Int = .random(in: 1...5)
+        let currentPage: Int = .random(in: 1...totalPages)
+        
+        return MoviesPage(
+            page: currentPage,
+            results: [uniqueMovie()],
+            totalResults: .random(in: 6...10),
+            totalPages: totalPages
+        )
+    }
+    
+    private func uniqueMovie() -> Movie {
+        Movie(
+            id: .random(in: 100000...200000),
+            title: "AnyTitle"
+        )
     }
 }
