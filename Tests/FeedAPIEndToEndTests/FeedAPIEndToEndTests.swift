f2e24dd436a0473e374b9e680b39b7f2b9663939
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-02 16:11:01 +0000

    Extracted helper method in URLSessionHTTPClientTests to improve test readability and flexibility

--- Tests/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ Tests/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -6,6 +6,19 @@ import XCTest
 class FeedAPIEndToEndTests: XCTestCase {
     
     func test_endToEndTestServerGETFeedResult_returnsMoviesPageAsJSON() {
+        switch getFeedResult() {
+        case let .success(moviesPage)?:
+            XCTAssertEqual(moviesPage.page, 1, "Expected 1 moviesPage in the response")
+        case let .failure(error)?:
+            XCTFail("Expected successful moviesPage result, got \(error) instead")
+        default:
+            XCTFail("Expected successful moviesPage result, got no result instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadMoviesPageResult? {
         let testServerURL = URL(string: "https://api.themoviedb.org/3/search/movie?api_key=\(Secrets.tmdbAPIKey)&query=test")!
         let client = URLSessionHTTPClient()
         let loader = RemoteMoviesPageLoader(url: testServerURL, client: client)
@@ -22,13 +35,6 @@ class FeedAPIEndToEndTests: XCTestCase {
         }
         wait(for: [expectation], timeout: 5.0)
         
-        switch receivedResult {
-        case let .success(moviesPage)?:
-            XCTAssertEqual(moviesPage.page, 1, "Expected 1 moviesPage in the response")
-        case let .failure(error)?:
-            XCTFail("Expected successful moviesPage result, got \(error) instead")
-        default:
-            XCTFail("Expected successful moviesPage result, got no result instead")
-        }
+        return receivedResult
     }
 }
