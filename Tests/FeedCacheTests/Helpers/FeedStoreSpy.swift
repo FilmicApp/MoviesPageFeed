0d3e62bfa8b46e91d46b949442cda2ede70fbe17
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-03 02:31:31 +0000

    Moved FeedStoreSpy to its own file for shared use in FeedCacheTests

---
+++ Tests/FeedCacheTests/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,49 @@
+import FeedCache
+import Foundation
+
+class FeedStoreSpy: FeedStore {
+    
+    enum ReceivedMessage: Equatable {
+        case deleteCachedFeed
+        case insert(CacheMoviesPage, Date)
+    }
+    
+    // MARK: - Private Properties
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    
+    // MARK: - FeedStore API
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func insert(_ moviesPage: CacheMoviesPage, timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(moviesPage, timestamp))
+    }
+    
+    // MARK: - Spy Methods
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
+
