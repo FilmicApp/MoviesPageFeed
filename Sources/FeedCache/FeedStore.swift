0574a6cf928549bb49cadd77e404c748e693f156
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-03 14:23:36 +0000

    Move cache models (CacheMovie and CacheMoviesPage) to their own files in a separate models folder for better organisation of code and improved folder hierarchy

--- Sources/FeedCache/FeedStore.swift
+++ Sources/FeedCache/FeedStore.swift
@@ -15,39 +15,3 @@ public protocol FeedStore {
     func insert(_ moviesPage: CacheMoviesPage, timestamp: Date, completion: @escaping InsertionCompletion)
     func retrieve(completion: @escaping RetrievalCompletion)
 }
-
-// TODO: Move these into own files
-
-public struct CacheMoviesPage: Equatable {
-    
-    // MARK: - Public Properties
-    
-    public let page: Int
-    public let results: [CacheMovie]
-    public let totalResults: Int
-    public let totalPages: Int
-    
-    // MARK: - Init
-    
-    public init(page: Int, results: [CacheMovie], totalResults: Int, totalPages: Int) {
-        self.page = page
-        self.results = results
-        self.totalResults = totalResults
-        self.totalPages = totalPages
-    }
-}
-
-public struct CacheMovie: Equatable {
-    
-    // MARK: - Public Properties
-    
-    public let id: Int
-    public let title: String
-    
-    // MARK: - Init
-    
-    public init(id: Int, title: String) {
-        self.id = id
-        self.title = title
-    }
-}
