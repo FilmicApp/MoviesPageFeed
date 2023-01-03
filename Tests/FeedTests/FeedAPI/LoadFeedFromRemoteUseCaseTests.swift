f365ea99b8d59a57f4ed8618eb7f6d2ec7e11bc1
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-03 00:21:49 +0000

    Removed reference to items in favour of MoviesPage

--- Tests/FeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/FeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
@@ -190,8 +190,8 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         
         sut.load { receivedResult in
             switch (receivedResult, expectedResult) {
-            case let (.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.success(receivedMoviesPage), .success(expectedMoviesPage)):
+                XCTAssertEqual(receivedMoviesPage, expectedMoviesPage, file: file, line: line)
                 
             case let (.failure(receivedError as RemoteMoviesPageLoader.Error), .failure(expectedError as RemoteMoviesPageLoader.Error)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
