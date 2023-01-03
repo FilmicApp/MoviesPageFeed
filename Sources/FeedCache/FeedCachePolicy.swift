20dcceedc6531c1176793ade87a204e5d34d6485
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-03 13:42:31 +0000

    Made `FeedCachePolicy` into enum, and explicitly set access to internal to clarify required access control to future developers

--- Sources/FeedCache/FeedCachePolicy.swift
+++ Sources/FeedCache/FeedCachePolicy.swift
@@ -1,20 +1,16 @@
 import Foundation
 
-final class FeedCachePolicy {
+internal enum FeedCachePolicy {
     
     // MARK: - Private Properties
     
     private static let calendar = Calendar(identifier: .gregorian)
 
     private static var maxCacheAgeInDays = 7
-    
-    // MARK: - Init
-    
-    private init() {}
-        
+            
     // MARK: - API
     
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
