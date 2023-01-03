ada49163d184091a98a4da8abb5a8ad299901103
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-03 00:04:19 +0000

    Move FeedAPI models (RemoteMoviesPage and RemoteMovie) to their own files in a Models folder for better hierarchy organisation and code organisation

--- Sources/Feed/FeedAPI/MoviesPageMapper.swift
+++ Sources/Feed/FeedAPI/MoviesPageMapper.swift
@@ -19,22 +19,3 @@ final class MoviesPageMapper {
         return moviesPageDTO
     }
 }
-
-struct RemoteMoviesPage: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case page
-        case results
-        case totalResults = "total_results"
-        case totalPages = "total_pages"
-    }
-    
-    let page: Int
-    let results: [RemoteMovie]
-    let totalResults: Int
-    let totalPages: Int
-}
-
-struct RemoteMovie: Decodable {
-    let id: Int
-    let title: String
-}
