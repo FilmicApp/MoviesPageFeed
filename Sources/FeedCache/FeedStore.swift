8c474fcbab199ff0570884d3e607d813eca9a515
Paul Willis <ios_dev@paulrobertwillis.com>
2023-01-18 15:09:06 +0000

    Add comments to document that completion handlers in any `FeedStore` implementation can be invoked in any thread. Clients are responsible for dispatching to appropriate threads, if needed

--- Sources/FeedCache/FeedStore.swift
+++ Sources/FeedCache/FeedStore.swift
@@ -10,8 +10,16 @@ public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrievedCachedFeedResult) -> Void
-
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatching to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatching to appropriate threads, if needed.
     func insert(_ moviesPage: CacheMoviesPage, timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible for dispatching to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
